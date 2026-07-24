7a44976a83558987ba715c263aff33707535386a
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2026-07-24 17:26:26 +0400

    increase photo upload limit

--- medartis/next.config.ts
+++ medartis/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['*.ngrok-free.app', 'localhost:3000'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // 👈 Bumps limit from 1MB to 10MB
+    },
+  },
 };
 
 export default nextConfig;
